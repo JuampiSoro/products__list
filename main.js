c852ff5a782d5f7c51172e76b0946f53189c993e
JuampiSoro <jpsoro@Juans-MacBook-Air.local>
2022-07-20 10:58:34 -0300

    JS complete

--- main.js
+++ main.js
@@ -3,6 +3,23 @@ const inputBtn = document.querySelector("#input__btn");
 const body = document.querySelector(".body")
 
 
+const addBtnRemove = () => {
+    const btnEachProduct = document.createElement("button");
+    btnEachProduct.classList.add("each__product__btn");
+    btnEachProduct.innerHTML = `<ion-icon name="close"></ion-icon>`
+
+    btnEachProduct.addEventListener("click", (e) => {
+        const item = e.target.parentElement;
+
+        // item.parentElement.style.display = "none";
+        item.parentElement.remove();
+        // body.removeChild(item);
+    })
+
+    return btnEachProduct;
+}
+
+
 
 inputBtn.addEventListener("click", () => {
     const inputValue = input.value;
@@ -20,10 +37,10 @@ inputBtn.addEventListener("click", () => {
         pEachProduct.innerText = inputValue;
         divEachProduct.append(pEachProduct);
 
-        const btnEachProduct = document.createElement("button");
-        btnEachProduct.classList.add("each__product__btn");
-        btnEachProduct.innerHTML = `<ion-icon name="close"></ion-icon>`
-        divEachProduct.append(btnEachProduct);
+
+        divEachProduct.append(addBtnRemove());
+
+        // divEachProduct.append(btnEachProduct);
 
         input.value = "";
     }
